ba07853d6e1a3dfd7d661885e71d367b12ad9ca4
Vikaspendharkar <105772769+Vikaspendharkar@users.noreply.github.com>
2022-05-19 08:59:33 +0530

    Create Variables_files.py

---
+++ RobotFramework/Variablesfiles/Variables_files.py
@@ -0,0 +1,17 @@
+Chrome_browser = "chrome"
+Login_url = ["https://op-web1.omniparcel.com/", "https://op-web2.omniparcel.com/", "https://op-web3.omniparcel.com/",
+             "https://op-web4.omniparcel.com/", "https://op-web5.omniparcel.com/"]
+
+
+Shiping_urlweb1 = ["https://op-web1.omniparcel.com/ShipmentReport", "https://op-web2.omniparcel.com/ShipmentReport",
+                   "https://op-web3.omniparcel.com/ShipmentReport", "https://op-web4.omniparcel.com/ShipmentReport",
+                   "https://op-web5.omniparcel.com/ShipmentReport"]
+
+# Switch_brw = "Omni Test"
+actusername = "Omni Test,"
+cpyconnotenumber = []
+MAWB = "555"
+MAWB_1 = "22233444"
+# USERNAME = "sstech.vikas@gmail.com"
+# PASSWORD = "Vikas@3231"
+RECIPIENT = "sstech.vikas@gmail.com"
